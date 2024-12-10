1895889f19f820b84314c8233d207e1c1956cbba
kolywat <maxnem93@gmail.com>
2024-12-10 18:04:13 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import Stats from "./components/Stats/Stats";
 import Main from "./components/Main/Main";
@@ -8,13 +8,14 @@ import axios from "axios";
 import "./App.css";
 
 const App = () => {
-    const [authStatus, setAuthStatus] = useState(null); // Для отображения статуса аутентификации
+    const [authStatus, setAuthStatus] = useState(null);
+    const [telegramData, setTelegramData] = useState(null); // Сохраним данные Telegram в состоянии
 
     const authenticateWithServer = async (telegramData) => {
         try {
             const response = await axios.post(
-                "https://cors-anywhere.herokuapp.com/https://14e8-2001-2020-4343-fe89-c0f9-af62-d0f8-5a7d.ngrok-free.app/bot_tg_back/api/login/index.php", // Замените на URL вашего сервера
-                {}, // Нет тела, данные передаются через заголовки
+                "https://cors-anywhere.herokuapp.com/https://14e8-2001-2020-4343-fe89-c0f9-af62-d0f8-5a7d.ngrok-free.app/bot_tg_back/api/login/index.php",
+                {},
                 {
                     headers: {
                         "Content-Type": "application/x-www-form-urlencoded",
@@ -37,19 +38,26 @@ const App = () => {
     };
 
     useEffect(() => {
-        // Получение данных Telegram Web App
         if (window.Telegram && window.Telegram.WebApp) {
             const telegramData = window.Telegram.WebApp.initData;
+            setTelegramData(telegramData); // Сохраняем данные в состоянии
             authenticateWithServer(telegramData);
             console.log("Telegram Init Data:", telegramData);
         }
     }, []);
+
     return (
         <div className="app">
             <Header />
             <Stats />
             <Main />
             <Footer />
+
+            {/* Блок для отображения telegramData */}
+            <div className="debug-area" style={{ padding: '20px', background: '#f9f9f9', border: '1px solid #ccc', marginTop: '20px' }}>
+                <h2>Telegram Init Data:</h2>
+                <pre>{JSON.stringify(telegramData, null, 2)}</pre>
+            </div>
         </div>
     );
 };
