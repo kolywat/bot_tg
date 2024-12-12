01156c12fab3440dc0e11381fe83bde7d4098c12
kolywat <maxnem93@gmail.com>
2024-12-12 20:49:59 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { retrieveLaunchParams } from "@telegram-apps/sdk"; // Импорт retrieveLaunchParams
 import Header from "./components/Header/Header";
 import Stats from "./components/Stats/Stats";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
-import { initInitData } from '@telegram-apps/sdk';
 import "./App.css";
 
 const App = () => {
@@ -46,7 +44,7 @@ const App = () => {
     };
 
     useEffect(() => {
-        const initData = window.telegram?.window?.initData; // Извлекаем initData через window.telegram
+        const initData = window.Telegram.WebApp.initData; // Извлекаем initData через window.telegram
         if (initData) {
             console.log("Telegram Init Data:", initData);
 
